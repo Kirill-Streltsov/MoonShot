3d4d05b4d3b74c947796a5357b0097e158ebc757
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-30 23:00:21 +0200

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Kirill Streltsov on 30.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
